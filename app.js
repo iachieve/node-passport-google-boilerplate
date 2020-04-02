ea31507877e0503cec7751e8fb98fff744ccc150
iachieve <mhalim80@hotmail.com>
2020-04-02 10:01:19 -0700

    configure production variables

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const PORT = process.env.PORT || 5000;
-
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
+const keys = require('./config/keys_dev');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const app = express();
@@ -23,9 +24,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // connect to mongodb
-mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true }, () => {
-  console.log('connected to mongodb');
-});
+mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+  .catch(err => console.log(err));
+mongoose.set('useCreateIndex', true);
+
 
 // set up routes
 app.use('/auth', authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./keys');
+const keys = require('./keys_dev');
 const User = require('../models/user-model');
 
 passport.serializeUser((user, done)=>{
